325429f680a1e66655685f7fa51ae48032ab13b7
adam <ad.amer1989@gmail.com>
2022-05-16 06:47:22 -0700

    edit live-mnist.py

--- webserver/app.py
+++ webserver/app.py
@@ -1,8 +1,23 @@
 import requests
 from flask import Flask, send_file, request, render_template
 import boto3
+import re
+import base64
+import io
+from PIL import Image
 
 app = Flask(__name__, static_url_path='')
+
+def getI420FromBase64(codec):
+    base64_data = re.sub('^data:image/.+;base64,', '', codec.decode())
+    byte_data = base64.b64decode(base64_data)
+    image_data = io.BytesIO(byte_data)
+    img = Image.open(image_data)
+    img.load()  # required for png.split()
+    background = Image.new("RGB", img.size, (255, 255, 255))
+    background.paste(img, mask=img.split()[3])  # 3 is the alpha channel
+    background.save('image.jpg', 'JPEG', quality=80)
+    img.save('image.png', "PNG")
 
 
 @app.route("/")
@@ -14,6 +29,7 @@ def home():
 def hello_world():
     data = request.data
     prediction = requests.get(f'http://mnist-predictor-service:8080/predict', data=data)
+    getI420FromBase64(data)
     s3_client = boto3.client('s3')
     s3_client.upload_file(data, 'adhambucket1', 'adham/my_image')
     return prediction.json()
